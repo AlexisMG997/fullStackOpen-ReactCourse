765eaed29b1ee17a9e2924ad27c1d1b5bbcaa111
Alexis <alexis@caret.mx>
2023-10-19 21:13:21 -0700

    Part1 - Exercise 1.2 completed!

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -6,12 +6,22 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
         {props.part} {props.exercise}
       </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
     </>
   );
 };
@@ -38,9 +48,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
       <Total
         exercise1={exercises1}
         exercise2={exercises2}
