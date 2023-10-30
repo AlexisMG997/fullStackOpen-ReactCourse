463485fa0058d5a77a2d5c18ca0bc7c8c373d668
Practicas WEB <practicasweb@tijuana.gob.mx>
2023-10-30 12:47:21 -0700

    Exercises 2.12 to 2.15 completed!

--- part2/thePhonebook/src/App.jsx
+++ part2/thePhonebook/src/App.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
-import axios from 'axios';
+import axios from "axios";
+import personService from "./services/person";
 
 function App() {
   const [persons, setPersons] = useState([]);
@@ -10,14 +11,17 @@ function App() {
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
 
+  // Obtaining data from RESTApi
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-    .then( response => {
-      const data = response.data;
-      setPersons(data);
-    })
+    // axios.get('http://localhost:3001/persons')
+    // .then( response => {
+    //   const data = response.data;
+    //   setPersons(data);
+    // })
+    personService.getAll().then((result) => setPersons(result));
   }, []);
 
+  // Adding new person
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
@@ -25,26 +29,55 @@ function App() {
       number: newNumber,
     };
     let copyPersons = persons;
+    let foundPerson = copyPersons.filter((p) => p.name === personObject.name);
     if (!copyPersons.find((x) => x.name === newName)) {
-      setPersons(persons.concat(personObject));
+      // setPersons(persons.concat(personObject));
+      personService
+        .create(personObject)
+        .then((newPerson) => setPersons(persons.concat(newPerson)));
       copyPersons = persons.concat(personObject);
-    } else {alert("Name cannot be the same that one from the list");}
+    } else {
+      const id = foundPerson[0].id;
+      confirm(
+        `${newName} is already added to the Phonebook, replace the old number with a new one?`
+      ) &&
+        personService
+          .update(foundPerson[0].id, personObject)
+          .then((result) =>
+            setPersons(
+              persons.map((person) =>
+                person.id !== id ? person : personObject
+              )
+            )
+          );
+    }
     setNewName("");
     setNewNumber("");
   };
 
+  // Deleting person
+  const deletePerson = (id) => {
+    console.log(`Person deleted with id number ${id}`);
+    personService
+      .remove(id)
+      .then((result) => setPersons(persons.filter((p) => p.id !== id)));
+  };
+
+  // Applying filters
   const filterResult = (event) => {
     console.log(event.target.value);
-    const value = event.target.value; 
+    const value = event.target.value;
     console.log(persons.find((x) => x.name === value || x.number === value));
     setFilter(persons.find((x) => x.name === value || x.number === value));
   };
 
+  // Adding person name
   const handlePersonChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
 
+  // Adding phone number
   const handleNumberChange = (event) => {
     console.log(event.target.value);
     setNewNumber(event.target.value);
@@ -53,16 +86,16 @@ function App() {
   return (
     <div>
       <h2>PhoneBook</h2>
-      <Filter filterResult={filterResult}/>
+      <Filter filterResult={filterResult} />
       <PersonForm
-      key={persons.length}
+        key={persons.length}
         addPerson={addPerson}
         handlePersonChange={handlePersonChange}
         handleNumberChange={handleNumberChange}
         newNum={newName}
         newNumber={newNumber}
       />
-      <Persons persons={persons} filters={filter} />
+      <Persons persons={persons} filters={filter} deletePerson={deletePerson} />
     </div>
   );
 }
--- part2/thePhonebook/src/components/Persons.jsx
+++ part2/thePhonebook/src/components/Persons.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
-const Persons = ({ persons, filters }) => {
+const Persons = ({ persons, filters, deletePerson }) => {
   const newFilter = [filters];
   return (
     <>
       <h2>Numbers</h2>
-      {filters == "" || filters == undefined? persons.map((person) => 
-        <p key={person.name}>{person.name} | {person.number}</p>
-      ) : 
-      newFilter.map((filter) => 
-        <p key={filter.number}>{filter.name} | {filter.number}</p>
-      )}
+      {filters == "" || filters == undefined
+        ? persons.map((person) => (
+            <p key={person.name}>
+              {person.name} | {person.number}{" "}
+              <button onClick={() => deletePerson(person.id)}>Delete</button>
+            </p>
+          ))
+        : newFilter.map((filter) => (
+            <p key={filter.number}>
+              {filter.name} | {filter.number}{" "}
+              <button onClick={() => deletePerson(filter.id)}>Delete</button>
+            </p>
+          ))}
     </>
   );
 };
