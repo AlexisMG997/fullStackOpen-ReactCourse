9de9b3ae99fe86c2e3f525b9fd2ab53395064f5d
Magallanes Campos <amagallanes@tijuana.gob.mx>
2023-11-07 14:41:49 -0800

    Exercise 2.20 completed!

--- part2/dataForCountries/src/components/Weather.jsx
+++ part2/dataForCountries/src/components/Weather.jsx
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const Weather = ({ latitude, longitude, country }) => {
   const [data, setData] = useState(null);
-  const apiKey = import.meta.env.VITE_weatherKey;
+  const apiKey = import.meta.env.VITE_SOME_KEY;
   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  let icon;
   console.log(apiKey);
 
   useEffect(() => {
@@ -13,14 +13,19 @@ const Weather = ({ latitude, longitude, country }) => {
       console.log(result.data);
       setData(result.data);
     });
+    console.log(icon);
   }, []);
 
   return (
     <>
-      <h3>Wheather in </h3>
-      <p>Temperature {data.main.temp} Celcius</p>
-      {data != null && <img src={icon} alt={data.weather[0].icon} />}
-      <p>Wind {data.wind.speed} m/s</p>
+      {data != null ?
+        <>
+          <h3>Wheather in </h3>
+          <p>Temperature {data.main.temp} Celcius</p>
+          {data != null && <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt={data.weather[0].icon} />}
+          <p>Wind {data.wind.speed} m/s</p>
+        </>
+        : ''}
     </>
   );
 };
